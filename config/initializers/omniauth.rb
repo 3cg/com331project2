919d4d096e671d7ef7f4ac2a9c60e79529992479
wxie <wxie11@hawk.iit.edu>
2019-04-09 09:00:40 -0500

    enable developer omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
   provider :github, ENV['GITHUB_IIT_KEY'], ENV['GITHUB_IIT_SECRET']
 end
