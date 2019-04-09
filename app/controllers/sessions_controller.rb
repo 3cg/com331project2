c784426924b163ee007350e13dd281e7377062d0
wxie <wxie11@hawk.iit.edu>
2019-04-09 14:06:43 -0500

    fix github omniauth connection issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   protect_from_forgery except: :create
   def create
-    # @provider_hash = request.env['omniauth.auth'].to_json
+  #  @provider_hash = request.env['omniauth.auth'].to_json
     omni = request.env['omniauth.auth']
     @reporter = Reporter.find_or_create_by(uid: omni['uid']) do |reporter|
-      reporter.first_name = omni['info']['first_name']
+      reporter.name = omni['info']['name']
       reporter.nickname = omni['info']['nickname']
       reporter.email = omni['info']['email']
     end
