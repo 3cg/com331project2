f9f20a37d0831c79253b3a935f6f597e64eb778e
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-04-09 19:07:34 -0500

    added name column, and displaying hash now. issue with email and name displaying

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   protect_from_forgery except: :create
   def create
-  #  @provider_hash = request.env['omniauth.auth'].to_json
+  @provider_hash = request.env['omniauth.auth'].to_json
     omni = request.env['omniauth.auth']
     @reporter = Reporter.find_or_create_by(uid: omni['uid']) do |reporter|
       reporter.name = omni['info']['name']
