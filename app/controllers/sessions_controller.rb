a1e6a0a52c5b1519b8c5f08b792401c95d6b708e
jhuang60 <jhuang60@hawk.iit.edu>
2019-04-11 21:07:06 -0500

    display image and fix to complain_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       reporter.email = omni['info']['email']
     end
       self.current_user = @reporter
-      redirect_to posts_url
+      redirect_to complaints_url
   end
 
   def login
