d4c341604c29a6af33267b229e78c7996da606f4
wxie <wxie11@hawk.iit.edu>
2019-04-18 18:21:10 -0500

    create new report account for first time users

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,9 +2,14 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get create" do
+  test "should redirect to posts URL on login" do
     post '/auth/github/callback', params: OmniAuth.config.mock_auth[:github]
     assert_redirected_to complaints_url
   end
 
+  test "first-time login should create a new Report record" do
+    assert_difference('Report.count') do
+      post '/auth/github/callback', params: OmniAuth.config.add_mock(:github, {uid: '22222'})
+    end
+  end
 end
