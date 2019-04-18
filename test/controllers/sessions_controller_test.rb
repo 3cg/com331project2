97becc178de2481b305ed95f3498e16f53b5f9f3
wxie <wxie11@hawk.iit.edu>
2019-04-18 18:23:55 -0500

    test login and logout with session[:uid]

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -8,8 +8,15 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "first-time login should create a new Report record" do
-    assert_difference('Report.count') do
+    assert_difference('Reporter.count') do
       post '/auth/github/callback', params: OmniAuth.config.add_mock(:github, {uid: '22222'})
+
+      assert_equal(session[:uid],'22222')
     end
+  end
+
+  test "user should be able to logout" do
+    get logout_url
+    assert_nil(session[:uid])
   end
 end
