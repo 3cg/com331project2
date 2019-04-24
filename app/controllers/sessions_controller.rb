595e7a5c8fd1e401317a0c809462c4040c363cfe
jdejesu2 <jdejesu2@hawk.iit.edu>
2019-04-24 18:08:33 -0500

    prevent redirecting to origin from login or logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,21 @@
 class SessionsController < ApplicationController
   protect_from_forgery except: :create
   def create
-  @provider_hash = request.env['omniauth.auth'].to_json
+    @provider_hash = request.env['omniauth.auth'].to_json
     omni = request.env['omniauth.auth']
     @reporter = Reporter.find_or_create_by(uid: omni['uid']) do |reporter|
       reporter.name = omni['info']['name']
       reporter.nickname = omni['info']['nickname']
       reporter.email = omni['info']['email']
     end
-      self.current_user = @reporter
-      redirect_to request.env['omniauth.origin'] || complaints_url
+    self.current_user = @reporter
+    origin = request.env['omniauth.origin']
+    redirect_to origin.include?('log') ? complaints_url : origin
   end
-
   def login
   end
   def logout
     reset_session
     redirect_to login_url, notice: "You have been logged out."
   end
-
 end
